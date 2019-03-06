b21950a2f72ff1b599dd6a0c2a8cc80ec53848f9
Cody-Boullt <48022559+Cody-Boullt@users.noreply.github.com>
2019-03-06 09:06:02 -0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,11 @@
+//Cody Boullt cboullt@cnm.edu BoulltE9
+
+//include necessary code libraries for later use
+#include <iostream>
+using namespace std;
+
+//declare functions
+void Header();
+void FillArray(int numbers[], int total);
+float AveArray(int numbers[], int total);
+float StdDeviation(int numbers[], int total, float average);
